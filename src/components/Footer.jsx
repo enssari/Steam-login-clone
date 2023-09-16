857d7708ba32f82e52cc94fc75b8ef2f6717640b
Enes Sari <terdodios@gmail.com>
2023-09-16 13:12:06 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer = () => {
     return (
-        <div className="flex flex-col justify-center text-center py-[7.1rem] w-100% mx-auto 2xl:flex-row" id="container-bottom">
+        <div className="flex flex-col justify-center text-center py-[7.1rem] w-full mx-auto 2xl:flex-row" id="container-bottom">
             <div className="container justify-center text-center text-white font-arial space-y-3 w-[13rem] relative left-[9.8rem] top-[-3rem] sm:left-[14rem] md:left-[23.5rem] lg:left-[34rem] xl:left-[36.8rem] 2xl:left-[0rem]" id="upper-container">
                 <h1 className='font-bold text-xl'>New to Steam?</h1>
                 <button className='w-[10.5rem] h-[2rem] rounded-sm bg-gradient-to-r from-blue-400 to-blue-600 hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-500'>Create an account</button>
